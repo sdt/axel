e69b89734824b31ed07e6e72bc58f7749a4657f4
Stephen Thirlwall <sdt@dr.com>
2016-05-31 11:27:36 +1000

    Switch SSLv23_client_method to TLS_client_method
    This needs further work as TLS_client_method isn't available in
    all versions of openssl

--- src/ssl.c
+++ src/ssl.c
@@ -56,7 +56,7 @@ void ssl_startup( void )
 	SSL_library_init();
 	SSL_load_error_strings();
 
-	ssl_ctx = SSL_CTX_new( SSLv23_client_method() );
+	ssl_ctx = SSL_CTX_new( TLS_client_method() );
 	if( !conf->insecure ) {
 		SSL_CTX_set_default_verify_paths(ssl_ctx);
 		SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_PEER, NULL);
